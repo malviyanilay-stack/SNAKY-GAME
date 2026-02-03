5bf02bc01381d459f9ebfc1756e69a0245831913
thenils projects <malviyanilay@gmail.com>
2026-02-03 20:09:26 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const GRID_SIZE = 40;
+const rows = GRID_SIZE, cols = GRID_SIZE;
+const scale = 17; // (You can decrease this for more visible play area!)
+canvas.width = cols * scale;
+canvas.height = rows * scale;
